5af682d5c4785871199aea61851fda3dedf615a7
Deepika Dubey <deepikadubey1209@gmail.com>
2024-01-26 01:01:11 +0530

    Splash Screen : SuccessFully Added.

--- app/src/main/java/com/example/myintrofolio/splash_screen.kt
+++ app/src/main/java/com/example/myintrofolio/splash_screen.kt
@@ -1,11 +1,19 @@
 package com.example.myintrofolio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import java.util.logging.Handler
 
 class splash_screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+        } ,4000)
+
     }
 }
