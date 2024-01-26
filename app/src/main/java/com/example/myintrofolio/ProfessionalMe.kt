6a3e4c7f899c75d46b7de86749fe495fbfddf37d
Deepika Dubey <deepikadubey1209@gmail.com>
2024-01-26 22:39:28 +0530

    Added three empty Activity .

---
+++ app/src/main/java/com/example/myintrofolio/ProfessionalMe.kt
@@ -0,0 +1,11 @@
+package com.example.myintrofolio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfessionalMe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_professional_me)
+    }
+}
