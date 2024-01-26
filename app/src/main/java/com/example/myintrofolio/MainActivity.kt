6b757df4f7e9d5b648f72d5a546b5c504a46f958
Deepika Dubey <deepikadubey1209@gmail.com>
2024-01-26 23:19:37 +0530

    About Activity: Simple Layout designed and Connected the Activity with Main Activity Successfully.

--- app/src/main/java/com/example/myintrofolio/MainActivity.kt
+++ app/src/main/java/com/example/myintrofolio/MainActivity.kt
@@ -1,11 +1,25 @@
- package com.example.myintrofolio
+package com.example.myintrofolio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize the button
+        val btnOpenActivity: Button = findViewById(R.id.btnabout)
+
+        // Set click listener for the button
+        btnOpenActivity.setOnClickListener {
+            // Add code to handle button click event
+            Intent(this,About::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
